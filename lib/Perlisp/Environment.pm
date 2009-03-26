f2bf08e7f886e0ca8f5f1462b8c8aaa3767c479b
Shawn M Moore <sartak@gmail.com>
2009-03-26 00:19:37 -0400

    Add Perlisp::Environment

---
+++ lib/Perlisp/Environment.pm
@@ -0,0 +1,49 @@
+package Perlisp::Environment;
+use Moose;
+use MooseX::AttributeHelpers;
+
+has parent => (
+    is        => 'ro',
+    isa       => 'Perlisp::Environment',
+    predicate => 'has_parent',
+);
+
+has bindings => (
+    metaclass => 'Collection::Hash',
+    is        => 'ro',
+    isa       => 'HashRef',
+    default   => sub { {} },
+    provides  => {
+        get    => '_lookup',
+        set    => 'bind',
+        exists => 'has_binding',
+    },
+);
+
+sub lookup {
+    my $self = shift;
+    my $name = shift;
+
+    return $self->_lookup($name)
+        if $self->has_binding($name);
+
+    return $self->parent->_lookup($name, @_)
+        if $self->has_parent;
+
+    return;
+}
+
+sub subenv {
+    my $self = shift;
+
+    my $subenv = blessed($self)->new(parent => $self);
+    $subenv->bind(@_);
+
+    return $subenv;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
